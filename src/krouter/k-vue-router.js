3bb7269f1b4656e423d7f473b80f583aa5289423
zqguo <gzhqiang123@gmail.com>
2020-01-06 10:25:33 +0800

    modify function method to static method

--- src/krouter/k-vue-router.js
+++ src/krouter/k-vue-router.js
@@ -68,26 +68,44 @@ class KVueRouter {
       }
     }
   }
+
+  static install(_Vue) {
+    Vue = _Vue
+
+    // 这里是根组件把router实例传进去了，挂载到$options属性上面
+    Vue.mixin({
+      beforeCreate() {
+        if (this.$options.router) {
+          Vue.prototype.$router = this.$options.router
+        }
+      }
+    })
+
+    // 实现两个全局的组件 router-link router-view
+    Vue.component('router-link', RouterLink)
+
+    Vue.component('router-view', RouterView)
+  }
 }
 
 // 静态方法, Vue.use会调用这个方法, 并且把Vue构造函数传过来, install方法this指向为当前KVueRouter实例
-KVueRouter.install = function(_Vue) {
-  // 保存构造函数
-  Vue = _Vue
+// KVueRouter.install = function(_Vue) {
+//   // 保存构造函数
+//   Vue = _Vue
 
-  // 这里是根组件把router实例传进去了，挂载到$options属性上面
-  Vue.mixin({
-    beforeCreate() {
-      if (this.$options.router) {
-        Vue.prototype.$router = this.$options.router
-      }
-    }
-  })
+//   // 这里是根组件把router实例传进去了，挂载到$options属性上面
+//   Vue.mixin({
+//     beforeCreate() {
+//       if (this.$options.router) {
+//         Vue.prototype.$router = this.$options.router
+//       }
+//     }
+//   })
 
-  // 实现两个全局的组件 router-link router-view
-  Vue.component('router-link', RouterLink)
+//   // 实现两个全局的组件 router-link router-view
+//   Vue.component('router-link', RouterLink)
 
-  Vue.component('router-view', RouterView)
-}
+//   Vue.component('router-view', RouterView)
+// }
 
 export default KVueRouter
